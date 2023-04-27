8e6eb3b28a8eecdd6d48bdcade675f741ee31ab1
tatestill <tatestill@yahoo.com>
2023-04-27 08:17:26 -0400

    Adds working link to about paragraph

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,4 +1,5 @@
 import { Row, Col } from "react-bootstrap"
+import { Link } from "react-router-dom"
 export function HomePage (){ 
     return(
         <Row className = "App-homepage">
@@ -8,10 +9,12 @@ export function HomePage (){
             </Row>
             <Row style={{justifyContent:"center", borderRadius: "5px"}}>
                 <div style={{fontSize: "16px", backgroundColor: "#b8c5e6", color:"black", maxWidth: "50%", borderRadius: "10px", textAlign: "left", textIndent: "20px"}}>
-                    <div style={{padding: "15px"}}><p>Hello, my name is Tate Still and I'm a Computer Science major in my final year at UCF. During my time at UCF, I've taken various coursees to expand my knowledge and abilities in Computer Science.
+                    <div style={{padding: "15px"}}><p style={{paddingBottom: "0px"}}>Hello, my name is Tate Still and I'm a Computer Science major in my final year at UCF. During my time at UCF, I've taken various coursees to expand my knowledge and abilities in Computer Science.
                     In classes such as Processes for Object Oriented Software Design and Senior Design, I practiced working in agile workflows and developing as part of a team. During Senior Design I worked as a part of a team for RealityFlow, a virtual reality scripting tool.
-                    My team was tasked with creating the website and mobile application for Reality Flow, along with the Social UI within the VR app. The website can be viewed at <a href="https://beta.realityflow.io/">beta.realityflow.io</a>.
-                    </p>
+                    My team was tasked with creating the website and mobile application for Reality Flow, along with the Social UI within the VR app. The link to the site can be found below: </p>
+                    
+                    <Link to = "https://beta.realityflow.io/" style={{paddingTop:"0px", paddingBottom: "100px"}}>https://beta.realityflow.io/</Link>.
+                    <p></p>
                     <p>After four years of UCF I have developed solid understanding of several CompSci tools. I have experience in the following languages: C, C#, Java, Javascript, C++, Python.
                         Other items I have developed profiency with: React, Bootstrap, MongoDB, TensorFlow/Keras, Artifical Intelligence, Unity, Node.js, MySQL, GraphQL, and MongoDB
                     </p>
